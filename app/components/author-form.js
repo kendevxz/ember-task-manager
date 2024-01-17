5b6ffaf65c86c4f39e16b33db7bb47907296ae7b
Kenny Devin <kendevxz@gmail.com>
2024-01-17 18:58:27 +0700

    form reset on auhtor fixed

--- app/components/author-form.js
+++ app/components/author-form.js
@@ -1,11 +1,12 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class AuthorFormComponent extends Component {
   @service store;
 
-  name = '';
+  @tracked name = '';
 
   @action
   handleNameChange(event) {
